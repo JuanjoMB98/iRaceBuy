df283ffcdaf5f3b2cb00e44c6911b9b00974df52
JuanjoMB98 <juanjomb98@gmail.com>
2024-05-01 02:36:43 +0200

    ADD: filtrar por varias ids

--- src/functions/seasonsUtils.ts
+++ src/functions/seasonsUtils.ts
@@ -1,61 +1,61 @@
 import { leerJSON, guardarJSON } from "./prepareDataSeason";
 
-function getSeason( newParams = {} ):any{
-
-    const season = leerJSON("src/data/seasonData.json")
+function getSeason(newParams = {}): any {
+    const season = leerJSON("src/data/seasonData.json");
 
     const defaultParams = {
-        id: false,
-        licencia: false,
-        track_id: false,
-        track: false
+        id: [],             // { id: ["4865", "4805"] }
+        licencia: false,    // { licencia: "2" }
+        track_id: false,    // { track_id: "243" }
+        track: false,       // { track: 'Volusia Speedway Park' }
     };
 
     const params = { ...defaultParams, ...newParams };
 
-
     var filter = "1 == 1";
     var filteredSeasons = {};
 
-
-    //Devolverá cualquier season que sea de la id filtrada
-    if (params.id !== false) {
-        console.log("Filtrando temporadas por id:", params.id);
-        filter = filter + " && element.id == " + params.id
+    // Devolverá cualquier season que sea de las IDs filtradas
+    if (params.id.length > 0) {
+        console.log("Filtrando temporadas por IDs:", params.id);
+        const idFilter = params.id.map(id => "element.id == " + id).join(" || ");
+        filter = filter + " && (" + idFilter + ")";
     }
 
     //Devolverá cualquier season que sea de la licencia filtrada
     if (params.licencia !== false) {
         console.log("Filtrando temporadas por licencia:", params.licencia);
-        filter = filter + " && element.licencia == " + params.licencia
+        filter = filter + " && element.licencia == " + params.licencia;
     }
 
     //Devolverá cualquier season que tenga este cicuito_id en su calendario
     if (params.track_id !== false) {
         console.log("Filtrando temporadas por circuito_id:", params.track_id);
-        filter = filter + " && element.calendario.some(cal => cal.track_id == " + params.track_id + ")"
+        filter =
+            filter +
+            " && element.calendario.some(cal => cal.track_id == " +
+            params.track_id +
+            ")";
     }
 
     //Devolverá cualquier season que tenga este cicuito en su calendario
     if (params.track !== false) {
         console.log("Filtrando temporadas por circuito:", params.track);
-        filter = filter + ` && element.calendario.some(cal => cal.track == "${params.track}")`;
+        filter =
+            filter +
+            ` && element.calendario.some(cal => cal.track == "${params.track}")`;
     }
 
-    
-    filteredSeasons = season.filter(element => eval(filter))
+    filteredSeasons = season.filter((element) => eval(filter));
 
     return filteredSeasons;
 }
 
 // Función para guardar datos en un archivo JSON
 export function test(): void {
-    console.log(
-        (getSeason({track_id:"498"}))
-    );
-    guardarJSON("src/data/seasonResultado.json", getSeason({licencia:1})) 
+    console.log(getSeason({ track: 'Volusia Speedway Park' }));
+    // guardarJSON("src/data/seasonResultado.json", getSeason({licencia:1}))
 }
 
-
 // Crear función para agrupar por licencias
-// Crear función para contar circuitos ( Trackid ) 
+// Crear función para contar circuitos ( Trackid )
